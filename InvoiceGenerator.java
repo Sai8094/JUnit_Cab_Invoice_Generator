220b8c16c13195e9ec2dc6baaaced53e7b3168e0
SAI GOPI A V <saiav9923@gmail.com>
2025-08-31 19:08:50 +0530

    multiple_rides

--- InvoiceGenerator.java
+++ InvoiceGenerator.java
@@ -6,8 +6,23 @@ public class InvoiceGenerator {
 	int minute = 1;
 	double minimumPrice = 5.0;
 
+	// Calaculate_Fare for single ride
 	public double calculateFare(double distance, int time) {
+		if (distance < 0 || time < 0)
+			throw new IllegalArgumentException("Distance and Time cannot be negative");
 		double fare = distance * cost + time * minute;
 		return Math.max(fare, minimumPrice);
 	}
+
+	// Calaculate_Fare for Multiple_Rides
+	public double calculateFare(Ride[] rides) {
+		if (rides == null) {
+			throw new IllegalArgumentException("Rides cannot be null");
+		}
+		double total = 0.0;
+		for (Ride ride : rides)
+			total += calculateFare(ride.distance, ride.time);
+		return total;
+	}
+
 }
